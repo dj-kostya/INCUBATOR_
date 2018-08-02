2d99d029d71ca5e3c2b180f3e7f8bf3230395aba
dj-kostya <nosorev_ka@gkl-kemerovo.ru>
2018-08-02 13:37:07 +0700

    Add files via upload

--- package.sql
+++ package.sql
@@ -1,115 +1,155 @@
-CREATE OR REPLACE PACKAGE STUDENT4.THING_3_1 
-is  
-  FUNCTION "GET_THING_3_1_1" 
-    (
-     p_message out varchar2,
-     p_status out number
-    )return SYS_REFCURSOR;
-  FUNCTION "GET_THING_3_1_1_PIPE"
-    (
-        p_message out varchar2,
-        p_status out number
-    )return t_THING_table pipelined;
-  FUNCTION "GET_THING_3_1_1_JSON" 
-    (
-      p_message out varchar2,
-      p_status out number
-    ) return clob;
-  function "ADD_THING_3_1_2" 
-    (  
-    p_NAME IN VARCHAR2, 
-    p_PRICE IN NUMBER,
-    p_message OUT varchar2
-    )return number;  
-  FUNCTION "EDIT_THING_3_1_3" (
-    p_ID          IN     NUMBER,
-    p_NEW_NAME    IN     VARCHAR2,
-    p_NEW_PRICE   IN     NUMBER,
-    p_message     OUT    VARCHAR2)
-    return number;
-    FUNCTION "DELETE_THING_3_1_4" 
-    ( 
-        p_ID IN NUMBER,
-        p_WHO_DEL IN VARCHAR2,
-        p_message out VARCHAR2 
-        
-    ) RETURN NUMBER;
-   
-end;
+CREATE OR REPLACE PACKAGE STUDENT4.THING_3_1
+IS
+    FUNCTION "GET_THING_3_1_1" (p_message OUT VARCHAR2, p_status OUT NUMBER)
+        RETURN SYS_REFCURSOR;
+
+    FUNCTION "GET_THING_3_1_1_PIPE" (p_message   OUT VARCHAR2,
+                                     p_status    OUT NUMBER)
+        RETURN t_THING_table
+        PIPELINED;
+
+    FUNCTION "GET_THING_3_1_1_JSON" (p_message   OUT VARCHAR2,
+                                     p_status    OUT NUMBER)
+        RETURN CLOB;
+
+    FUNCTION "ADD_THING_3_1_2" (p_NAME      IN     VARCHAR2,
+                                p_PRICE     IN     NUMBER,
+                                p_message      OUT VARCHAR2)
+        RETURN NUMBER;
+
+    FUNCTION "EDIT_THING_3_1_3" (p_ID          IN     NUMBER,
+                                 p_NEW_NAME    IN     VARCHAR2,
+                                 p_NEW_PRICE   IN     NUMBER,
+                                 p_message        OUT VARCHAR2)
+        RETURN NUMBER;
+
+    FUNCTION "DELETE_THING_3_1_4" (p_ID        IN     NUMBER,
+                                   p_WHO_DEL   IN     VARCHAR2,
+                                   p_message      OUT VARCHAR2)
+        RETURN NUMBER;
+END;
 /
-CREATE OR REPLACE PACKAGE STUDENT4.USERS_3_2 AS
-    FUNCTION "LOG_IN_3_2_1" 
-    (  
-        p_NAME IN VARCHAR2, 
-        p_PASSWORD IN VARCHAR2, 
-        p_message out varchar2
-    )  return number;
-    FUNCTION "EDIT_USER_3_2_2" 
-    (
-        p_ID IN NUMBER , 
-        p_NEW_NAME IN VARCHAR2 , 
-        p_NEW_PASS IN VARCHAR2, 
-        p_message out varchar2
-    ) return number;
-    FUNCTION "DEL_USER_3_2_3" 
-    ( 
-     P_ID_USER IN NUMBER , 
-     P_WHO_DEL IN varchar2 , 
-     p_message out varchar2 
-     ) return number;
-    FUNCTION "ADD_THING_TO_CART_3_2_4_1" 
-    ( 
-        p_ID IN NUMBER ,
-        p_thing IN NUMBER , 
-        p_message out varchar2 
-    ) return number;
-    FUNCTION "DEL_THING_FROM_CART_3_2_4_2" 
-    (  
-    p_ORDER_ IN NUMBER , 
-    p_THING IN NUMBER , 
-    p_WHO_DEL in varchar2,
-    p_message out varchar2 
-    ) return number;
-    function "ADD_DELIVERY_3_2_5" (
-    p_SURNAME    IN     VARCHAR2,
-    p_NAME       IN     VARCHAR2,
-    p_PATRONYMIC IN     VARCHAR2,
-    p_ADRESS     IN     VARCHAR2,
-    p_TELEFON    IN     VARCHAR2,
-    p_ID_ORDER   IN     NUMBER,
-    p_CITY       IN     NUMBER,
-    p_message    out    varchar2 
-    ) return number;
-    function "GET_ORDERS_3_2_6" 
-    (
-        p_id_usr number,
-        p_status OUT number,
-        p_message    out    varchar2 
-    ) return SYS_REFCURSOR;
-    function "GET_ORDERS_3_2_6_JSON" 
-    (
-    p_id_usr number,
-    p_message    out    varchar2,
-    p_status OUT number
-    ) return clob;
-    FUNCTION "GET_ORDERS_3_2_6_PIPE" 
-    (
-    p_id_usr number,
-    p_message    out    varchar2,
-    p_status OUT number
-    ) return t_orders_table pipelined;
-    function ADD_ORDER_3_2_7 
-    (
-        p_id_usr number,
-        p_status_order varchar2,
-        p_message out varchar2 
-    ) return number;
-    function DEL_ORDER_3_2_7 
-    (
-        p_order number,
-        p_status_order varchar2,
-        p_del_user varchar2,
-        p_message out varchar2 
-    ) return number;
-END ;
+CREATE OR REPLACE PACKAGE STUDENT4.USERS_3_2
+AS
+    FUNCTION "LOG_IN_3_2_1" (p_NAME       IN     VARCHAR2,
+                             p_PASSWORD   IN     VARCHAR2,
+                             p_message       OUT VARCHAR2)
+        RETURN NUMBER;
+
+    FUNCTION "EDIT_USER_3_2_2" (p_ID         IN     NUMBER,
+                                p_NEW_NAME   IN     VARCHAR2,
+                                p_NEW_PASS   IN     VARCHAR2,
+                                p_message       OUT VARCHAR2)
+        RETURN NUMBER;
+
+    FUNCTION "DEL_USER_3_2_3" (P_ID_USER   IN     NUMBER,
+                               P_WHO_DEL   IN     VARCHAR2,
+                               p_message      OUT VARCHAR2)
+        RETURN NUMBER;
+
+    FUNCTION "ADD_THING_TO_CART_3_2_4_1" (p_ID_order   IN     NUMBER,
+                                          p_thing      IN     NUMBER,
+                                          p_message       OUT VARCHAR2)
+        RETURN NUMBER;
+
+    FUNCTION "DEL_THING_FROM_CART_3_2_4_2" (p_ORDER_    IN     NUMBER,
+                                            p_THING     IN     NUMBER,
+                                            p_WHO_DEL   IN     VARCHAR2,
+                                            p_message      OUT VARCHAR2)
+        RETURN NUMBER;
+
+    FUNCTION "ADD_DELIVERY_3_2_5" (
+        p_SURNAME      IN     VARCHAR2 DEFAULT NULL,
+        p_NAME         IN     VARCHAR2 DEFAULT NULL,
+        p_PATRONYMIC   IN     VARCHAR2 DEFAULT NULL,
+        p_ADRESS       IN     VARCHAR2 DEFAULT NULL,
+        p_TELEFON      IN     VARCHAR2 DEFAULT NULL,
+        p_ID_ORDER     IN     NUMBER,
+        p_CITY         IN     NUMBER,
+        p_message         OUT VARCHAR2)
+        RETURN NUMBER;
+
+    FUNCTION "GET_ORDERS_3_2_6" (p_id_usr        NUMBER,
+                                 p_status    OUT NUMBER,
+                                 p_message   OUT VARCHAR2)
+        RETURN SYS_REFCURSOR;
+
+    FUNCTION "GET_ORDERS_3_2_6_JSON" (p_id_usr        NUMBER,
+                                      p_message   OUT VARCHAR2,
+                                      p_status    OUT NUMBER)
+        RETURN CLOB;
+
+    FUNCTION "GET_ORDERS_3_2_6_PIPE" (p_id_usr        NUMBER,
+                                      p_message   OUT VARCHAR2,
+                                      p_status    OUT NUMBER)
+        RETURN t_orders_table
+        PIPELINED;
+
+    FUNCTION ADD_ORDER_3_2_7 (p_id_usr             NUMBER,
+                              p_status_order       VARCHAR2,
+                              p_message        OUT VARCHAR2)
+        RETURN NUMBER;
+
+    FUNCTION DEL_ORDER_3_2_7 (p_order              NUMBER,
+                              p_status_order       VARCHAR2,
+                              p_del_user           VARCHAR2,
+                              p_message        OUT VARCHAR2)
+        RETURN NUMBER;
+END;
+/
+CREATE OR REPLACE PACKAGE STUDENT4.WAREHOUSE_3_3
+AS
+    FUNCTION "ADD_WAREHOUSE_3_3_1" (p_city          NUMBER,
+                                    p_adress        VARCHAR2,
+                                    p_message   OUT VARCHAR2)
+        RETURN NUMBER;
+
+    FUNCTION "DEL_WAREHOUSE_3_3_1" (p_id            NUMBER,
+                                    p_WHO_DEL       VARCHAR2,
+                                    p_message   OUT VARCHAR2)
+        RETURN NUMBER;
+    FUNCTION "TRANS_FROM_WAREHOUSE_3_3_3" (P_WAREHOUSE_IN        NUMBER,
+                                         P_WAREHOUSE_OUT       NUMBER,
+                                         P_THING               NUMBER,
+                                         p_quantity            NUMBER,
+                                         p_message         OUT VARCHAR2 -- 0-все хорошо 1- не сработало(
+                                                                       )
+        RETURN NUMBER;    
+END WAREHOUSE_3_3;
+/
+CREATE OR REPLACE PACKAGE STUDENT4.Reports_3_4
+AS
+    FUNCTION GET_delivery_3_4_1_pipe (p_status    OUT NUMBER,
+                                      p_message   OUT VARCHAR2)
+        RETURN t_delivery_table
+        PIPELINED;
+
+
+    FUNCTION GET_delivery_3_4_1 (p_status OUT NUMBER, p_message OUT VARCHAR2)
+        RETURN SYS_REFCURSOR;
+
+    FUNCTION GET_delivery_3_4_1_JSON (p_status    OUT NUMBER,
+                                      p_message   OUT VARCHAR2)
+        RETURN CLOB;
+
+    /***************************************************************************
+
+    ***************************************************************************/
+
+    FUNCTION get_quantity_3_4_2_PIPE (p_quantity       NUMBER,
+                                      p_status     OUT NUMBER,
+                                      p_message    OUT VARCHAR2)
+        RETURN t_quantity_table
+        PIPELINED;
+
+    FUNCTION get_quantity_3_4_2_JSON (p_quantity       NUMBER,
+                                      p_status     OUT NUMBER,
+                                      p_message    OUT VARCHAR2)
+        RETURN CLOB;
+
+    FUNCTION get_quantity_3_4_2 (p_quantity       NUMBER,
+                                 p_status     OUT NUMBER,
+                                 p_message    OUT VARCHAR2)
+        RETURN SYS_REFCURSOR;
+END Reports_3_4;
 /
